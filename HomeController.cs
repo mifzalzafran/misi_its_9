f8ae1b2b4084efe86b6a545270a9e46cea9f0b90
mifzalzafran <mifzalgames.1059@gmail.com>
2025-11-08 15:02:56 +0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("rumah")]
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, welocme to my API!";
+        }
+
+        [HttpGet("hai")]
+
+        public string Hai([FromQuery] string nama)
+
+        {
+            return "Hello, " + nama + "!";
+        }
+    }
+}
